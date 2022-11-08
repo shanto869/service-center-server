d6704d2e10f361334f5e633b994447d32e454679
shanto869 <shantobiswas869@gmail.com>
2022-11-08 19:54:25 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -7,6 +9,12 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+
+// mongodb 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.17kk4sr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
 
 app.get("/", (req, res) => {
     res.send('server is running')
