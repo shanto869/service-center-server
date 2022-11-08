b1bdc52e548efc0c3e57935e3835782b5d08bb5a
shanto869 <shantobiswas869@gmail.com>
2022-11-08 20:05:42 +0600

    get services from db

--- index.js
+++ index.js
@@ -14,7 +14,28 @@ app.use(express.json())
 // mongodb 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.17kk4sr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
+
+async function run() {
+    try {
+        const serviceCollection = client.db('Fitness-Cube').collection('services')
+
+        // get services from db
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err))
+
 
 app.get("/", (req, res) => {
     res.send('server is running')
